523dfcd15b2892f16207c1d0a18394d5218dea72
Antony Pavlov <antonynpavlov@gmail.com>
2013-05-09 12:26:22 +0400

    MIPS: XBurst: make possible pinctrl in JZ4755 boards

--- arch/mips/mach-xburst/devices-jz4755.c
+++ arch/mips/mach-xburst/devices-jz4755.c
@@ -1,10 +1,13 @@
 #include <common.h>
 #include <init.h>
 #include <mach/jz4750d_regs.h>
+#include <mach/pinctrl.h>
 
 static int jz4755_gpio_init(void)
 {
 	int i;
+
+	jz_pinctrl_init((void *)JZ4755_GPIO_BASE_ADDR);
 
 	for (i = 0; i < 6; i++) {
 		add_generic_device("jz-gpio", i, NULL,
