8ba761f5ed5a076eb681ea735f49e3f9b4fe2488
Antony Pavlov <antonynpavlov@gmail.com>
2013-05-10 23:16:02 +0400

    compile

--- arch/mips/mach-xburst/devices-jz4755.c
+++ arch/mips/mach-xburst/devices-jz4755.c
@@ -15,6 +15,16 @@ static int jz4755_gpio_init(void)
 				0x100, IORESOURCE_MEM, NULL);
 	}
 
+#define JZ_GPIO_UART1_TXD		JZ_GPIO_PORTE(25)
+#define JZ_GPIO_UART1_RXD		JZ_GPIO_PORTE(23)
+
+/* PFUN=1 PTRG=0 PSEL=1 */
+#define JZ_GPIO_FUNC_UART1_TXD		JZ_GPIO_FUNC2
+#define JZ_GPIO_FUNC_UART1_RXD		JZ_GPIO_FUNC2
+
+	jz_gpio_enable_pullup(JZ_GPIO_UART1_TXD);
+	jz_gpio_set_function(JZ_GPIO_UART1_TXD, JZ_GPIO_FUNC_UART1_TXD);
+
 	return 0;
 }
 coredevice_initcall(jz4755_gpio_init);
--- arch/mips/mach-xburst/include/mach/pinctrl.h
+++ arch/mips/mach-xburst/include/mach/pinctrl.h
@@ -1,6 +1,16 @@
 #ifndef __MACH_PINCTRL_H__
 #define __MACH_PINCTRL_H__
 
+/* Just all modern Ingenic SoCs have port A-D */
+#define	JZ_GPIO_PORTA(pin)	(0x00 + pin)
+#define	JZ_GPIO_PORTB(pin)	(0x20 + pin)
+#define	JZ_GPIO_PORTC(pin)	(0x40 + pin)
+#define	JZ_GPIO_PORTD(pin)	(0x60 + pin)
+
+/* JZ4755 and JZ4770 have ports E and F */
+#define	JZ_GPIO_PORTE(pin)	(0x80 + pin)
+#define	JZ_GPIO_PORTF(pin)	(0xa0 + pin)
+
 enum jz_gpio_function {
 	JZ_GPIO_FUNC_NONE,
 	JZ_GPIO_FUNC1,
@@ -9,5 +19,53 @@ enum jz_gpio_function {
 };
 
 void jz_pinctrl_init(void __iomem *jz_base);
+
+/*
+ Usually a driver for a SoC component has to request several gpio pins and
+ configure them as funcion pins.
+ jz_gpio_bulk_request can be used to ease this process.
+ Usually one would do something like:
+
+ static const struct jz_gpio_bulk_request i2c_pins[] = {
+	JZ_GPIO_BULK_PIN(I2C_SDA),
+	JZ_GPIO_BULK_PIN(I2C_SCK),
+ };
+
+ inside the probe function:
+
+    ret = jz_gpio_bulk_request(i2c_pins, ARRAY_SIZE(i2c_pins));
+    if (ret) {
+	...
+
+ inside the remove function:
+
+    jz_gpio_bulk_free(i2c_pins, ARRAY_SIZE(i2c_pins));
+
+*/
+
+struct jz_gpio_bulk_request {
+	int gpio;
+	const char *name;
+	enum jz_gpio_function function;
+};
+
+#define JZ_GPIO_BULK_PIN(pin) { \
+    .gpio = JZ_GPIO_ ## pin, \
+    .name = #pin, \
+    .function = JZ_GPIO_FUNC_ ## pin \
+}
+
+int jz_gpio_bulk_request(const struct jz_gpio_bulk_request *request, size_t num);
+void jz_gpio_bulk_free(const struct jz_gpio_bulk_request *request, size_t num);
+void jz_gpio_enable_pullup(unsigned gpio);
+void jz_gpio_disable_pullup(unsigned gpio);
+int jz_gpio_set_function(int gpio, enum jz_gpio_function function);
+
+#if 0
+int jz_gpio_port_direction_input(int port, uint32_t mask);
+int jz_gpio_port_direction_output(int port, uint32_t mask);
+void jz_gpio_port_set_value(int port, uint32_t value, uint32_t mask);
+uint32_t jz_gpio_port_get_value(int port, uint32_t mask);
+#endif
 
 #endif /* __MACH_PINCTRL_H__ */
