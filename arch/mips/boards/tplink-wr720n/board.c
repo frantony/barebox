21d6545421d1e710756cae1fa1b2d37cd563e6f5
Du Huanpeng <u74147@gmail.com>
2013-12-25 23:52:33 +0400

    MIPS: ar933x: add tplink-wr720n board support
    FIXME: add documentation
    
    Signed-off-by: Du Huanpeng <u74147@gmail.com>
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

---
+++ arch/mips/boards/tplink-wr720n/board.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2013 Du Huanpeng <u74147@gmail.com>
+ *
+ * This file is part of barebox.
+ * See file CREDITS for list of people who contributed to this project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <common.h>
+#include <init.h>
+#include <sizes.h>
+#include <asm/memory.h>
+
+static int model_hostname_init(void)
+{
+	barebox_set_model("TP Link WR720N");
+	barebox_set_hostname("wr720n");
+
+	return 0;
+}
+postcore_initcall(model_hostname_init);
+
+static int mem_init(void)
+{
+	mips_add_ram0(SZ_32M);
+
+	return 0;
+}
+mem_initcall(mem_init);
