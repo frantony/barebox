3021cdbe1c75b9b6604d38ada52341c7d371e48a
Oleksij Rempel <linux@rempel-privat.de>
2013-12-26 10:20:13 +0100

    add mach/pbl_macros.h for 9331
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

---
+++ arch/mips/mach-ar9331/include/mach/pbl_macros.h
@@ -0,0 +1,8 @@
+#ifndef __ASM_MACH_AR9331_PBL_MACROS_H
+#define __ASM_MACH_AR9331_PBL_MACROS_H
+
+#include <asm/regdef.h>
+#include <mach/ar71xx_regs.h>
+
+
+#endif /* __ASM_MACH_AR9331_PBL_MACROS_H */
