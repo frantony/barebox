44c410801366ecae36308cf553a92d951209d070
Antony Pavlov <antonynpavlov@gmail.com>
2014-02-23 02:14:19 +0400

    ARM: initial support for TI Davinci SoCs
    This commit adds minimal support for the Davinci 655 SoCs
    from Texas Instruments.
    
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

---
+++ arch/arm/mach-davinci/reset.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2013 Jean-Christophe PLAGNIOL-VILLARD <plagnio@jcrosoft.com>
+ *
+ * GPLv2 only
+ */
+
+#include <common.h>
+#include <io.h>
+
+void __noreturn reset_cpu(ulong addr)
+{
+	while(1);
+}
