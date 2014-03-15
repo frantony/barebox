f75343559629992059fe5c514328fec1fbc6052b
Antony Pavlov <antonynpavlov@gmail.com>
2014-03-15 11:59:05 +0400

    virt2real: switch to devicetree
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- arch/arm/boards/virt2real/board.c
+++ arch/arm/boards/virt2real/board.c
@@ -17,33 +17,11 @@
 
 #include <common.h>
 #include <init.h>
-#include <sizes.h>
-#include <asm/memory.h>
-#include <ns16550.h>
 
-#include <mach/serial.h>
-
-static int davinci_init(void)
+static int hostname_init(void)
 {
-	arm_add_mem_device("ram0", 0x82000000, SZ_16M);
-
-	return 0;
-}
-mem_initcall(davinci_init);
-
-static struct NS16550_plat serial_plat = {
-	.clock = 24000000,
-	.shift = 2,
-};
-
-static int console_init(void)
-{
-	barebox_set_model("virt2real");
 	barebox_set_hostname("virt2real");
 
-	add_ns16550_device(DEVICE_ID_DYNAMIC, DAVINCI_UART0_BASE,
-		8 << serial_plat.shift, IORESOURCE_MEM_8BIT, &serial_plat);
-
 	return 0;
 }
-console_initcall(console_init);
+core_initcall(hostname_init);
