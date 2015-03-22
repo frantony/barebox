1468029805bd6d181feeebaa065c4846b1afe343
Antony Pavlov <antonynpavlov@gmail.com>
2015-03-22 16:07:29 +0300

    MIPS: xburst: debug_ll: add JZ4770 support
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

---
+++ arch/mips/mach-xburst/include/mach/debug_ll_jz4770.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2015 Antony Pavlov <antonynpavlov@gmail.com>
+ *
+ * This file is part of barebox.
+ * See file CREDITS for list of people who contributed to this project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __INCLUDE_DEBUG_LL_JZ4770_H__
+#define __INCLUDE_DEBUG_LL_JZ4770_H__
+
+#include <mach/jz4770.h>
+
+#ifdef CONFIG_JZ47XX_DEBUG_LL_UART0
+#define DEBUG_LL_UART_ADDR	JZ4770_UART0_BASE
+#endif
+
+#ifdef CONFIG_JZ47XX_DEBUG_LL_UART1
+#define DEBUG_LL_UART_ADDR	JZ4770_UART1_BASE
+#endif
+
+#ifdef CONFIG_JZ47XX_DEBUG_LL_UART2
+#define DEBUG_LL_UART_ADDR	JZ4770_UART2_BASE
+#endif
+
+#ifdef CONFIG_JZ47XX_DEBUG_LL_UART3
+#define DEBUG_LL_UART_ADDR	JZ4770_UART3_BASE
+#endif
+
+#define DEBUG_LL_UART_SHIFT	2
+
+#ifndef DEBUG_LL_UART_CLK
+#define DEBUG_LL_UART_CLK	(12000000 / 16)
+#endif
+#define DEBUG_LL_UART_BPS	CONFIG_BAUDRATE
+#define DEBUG_LL_UART_DIVISOR	(DEBUG_LL_UART_CLK / DEBUG_LL_UART_BPS)
+
+#endif /* __INCLUDE_DEBUG_LL_JZ4770_H__ */
---
+++ arch/mips/mach-xburst/include/mach/jz4770.h
@@ -0,0 +1,25 @@
+/*
+ * JZ4770 SoC definitions
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __MIPS_ASM_MACH_JZ4770_JZ4770_H__
+#define __MIPS_ASM_MACH_JZ4770_JZ4770_H__
+
+/* APB bus devices */
+#define JZ4770_UART0_BASE	0xb0030000
+#define JZ4770_UART1_BASE	0xb0031000
+#define JZ4770_UART2_BASE	0xb0032000
+#define JZ4770_UART3_BASE	0xb0033000
+
+#endif /* __MIPS_ASM_MACH_JZ4770_JZ4770_H__ */
