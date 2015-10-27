0d6e241fa6f46f53489d9c59d75293debbed6352
Peter Mamonov <pmamonov@gmail.com>
2015-10-27 17:27:43 +0300

    WIP: mips: implement dma_sync_* functions

--- arch/mips/lib/dma-default.c
+++ arch/mips/lib/dma-default.c
@@ -16,13 +16,45 @@
  */
 
 #include <dma.h>
+#include <asm/io.h>
+
+#if	defined(CONFIG_CPU_MIPS32) || \
+	defined(CONFIG_CPU_MIPS64)
+static inline void __dma_sync_mips(unsigned long addr, size_t size,
+				   enum dma_data_direction direction)
+{
+	switch (direction) {
+	case DMA_TO_DEVICE:
+		dma_flush_range(addr, addr + size);
+		break;
+
+	case DMA_FROM_DEVICE:
+		dma_inv_range(addr, addr + size);
+		break;
+
+	case DMA_BIDIRECTIONAL:
+		dma_flush_range(addr, addr + size);
+		break;
+
+	default:
+		BUG();
+	}
+}
+#else
+static inline void __dma_sync_mips(void *addr, size_t size,
+	enum dma_data_direction direction)
+{
+}
+#endif
 
 void dma_sync_single_for_cpu(unsigned long address, size_t size,
 			     enum dma_data_direction dir)
 {
+	__dma_sync_mips(address, size, dir);
 }
 
 void dma_sync_single_for_device(unsigned long address, size_t size,
 				enum dma_data_direction dir)
 {
+	__dma_sync_mips(address, size, dir);
 }
