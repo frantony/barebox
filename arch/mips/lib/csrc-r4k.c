a159bc8ab9d2b431e74e6bc331915fc56b4e953d
Antony Pavlov <antonynpavlov@gmail.com>
2015-11-20 00:48:57 +0300

    WIP: MIPS: csrc-r4k: get cp0 counter rate from clk
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- arch/mips/lib/csrc-r4k.c
+++ arch/mips/lib/csrc-r4k.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Antony Pavlov <antonynpavlov@gmail.com>
+ * Copyright (C) 2011, 2015 Antony Pavlov <antonynpavlov@gmail.com>
  *
  * This file is part of barebox.
  * See file CREDITS for list of people who contributed to this project.
@@ -20,9 +20,11 @@
  * @brief Clocksource based on MIPS CP0 timer
  */
 
+#include <common.h>
 #include <init.h>
+#include <linux/clk.h>
+#include <linux/err.h>
 #include <clock.h>
-#include <io.h>
 #include <asm/mipsregs.h>
 
 static uint64_t c0_hpt_read(void)
@@ -30,15 +32,47 @@ static uint64_t c0_hpt_read(void)
 	return read_c0_count();
 }
 
-static struct clocksource cs = {
+static struct clocksource mips_r4k_cs = {
 	.read	= c0_hpt_read,
 	.mask	= CLOCKSOURCE_MASK(32),
 };
 
-static int clocksource_init(void)
+static int mips_r4k_timer_probe(struct device_d *dev)
 {
-	cs.mult = clocksource_hz2mult(100000000, cs.shift);
+	struct clk *timer_clk;
+	unsigned long rate;
 
-	return init_clock(&cs);
+	timer_clk = clk_get(dev, NULL);
+	if (IS_ERR(timer_clk)) {
+		int ret = PTR_ERR(timer_clk);
+		dev_err(dev, "clock not found: %d\n", ret);
+		return ret;
+	}
+
+	rate = clk_get_rate(timer_clk);
+
+	clocks_calc_mult_shift(&mips_r4k_cs.mult, &mips_r4k_cs.shift,
+		rate, NSEC_PER_SEC, 10);
+
+	return init_clock(&mips_r4k_cs);
 }
-core_initcall(clocksource_init);
+
+static __maybe_unused struct of_device_id mips_r4k_timer_dt_ids[] = {
+	{
+		.compatible = "mti,r4k-timer",
+	}, {
+		/* sentinel */
+	}
+};
+
+static struct driver_d mips_r4k_timer_driver = {
+	.probe	= mips_r4k_timer_probe,
+	.name	= "mips-r4k-timer",
+	.of_compatible = DRV_OF_COMPAT(mips_r4k_timer_dt_ids),
+};
+
+static int mips_r4k_clocksource_init(void)
+{
+	return platform_driver_register(&mips_r4k_timer_driver);
+}
+coredevice_initcall(mips_r4k_clocksource_init);
