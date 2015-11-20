6ed55fbef53e94ec2c91e9a9f801507571fcf061
Antony Pavlov <antonynpavlov@gmail.com>
2015-11-21 01:46:17 +0300

    WIP: MIPS: initial MediaTek MT7620 and ASUS RT-N14U support
    Build HOWTO
    ===========
    
    ````
    $ git clone -b 20151121.mt7620 https://github.com/frantony/barebox
    $ cd barebox
    $ export ARCH=mips
    $ export CROSS_COMPILE=<path to your compiler>/mips-linux-gnu-
    $ make asus-rt-n14u_defconfig
    $ make -j3
    ````
    
    barebox loadable binary image file is arch/mips/pbl/zbarebox.bin
    
    Starting barebox on ASUS RT-N14U board
    ======================================
    
    1. connect UART and turn board on
    ---------------------------------
    
    ````
    U-Boot 1.1.3 (Dec  6 2012 - 19:51:50)
    
    Board: Ralink APSoC DRAM:  64 MB
    relocate_code Pointer at: 83fb0000
    enable ephy clock...done. rf reg 29 = 5
    SSC enabled. swing=5000, upperbound=0
    ...
    Please choose the operation:
       0: Load system code then write to Flash via Serial.
       1: Load system code to SDRAM via TFTP.
       2: Load system code then write to Flash via TFTP.
       3: Boot system code via Flash (default).
       4: Entr boot command line interface.
       5: Load Boot Loader code to SDRAM via Serial.
       7: Load Boot Loader code then write to Flash via Serial.
       8: Load Boot Loader code to SDRAM via TFTP.
       9: Load Boot Loader code then write to Flash via TFTP.
    
    You choosed 4
    ````
    
    2. load zbarebox.bin via kermit and start it
    --------------------------------------------
    
    ````
    MT7620 # loadb
    
    MT7620 # go 0x80100000
    pbl_main_entry()
    Switch to console [cs0]
    
    barebox 2015.11.0-00169-ga44e462-dirty #203 Sat Nov 21 01:37:34 MSK 2015
    
    Board: ASUS RT-N14U
    Warning: Using dummy clocksource
    malloc space: 0xa0400000 -> 0xa07fffff (size 4 MiB)
    environment load /dev/env0: No such file or directory
    Maybe you have to create the partition.
    running /env/bin/init...
    /env/bin/init not found
    barebox:/
    ````
    
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

---
+++ arch/mips/boards/asus-rt-n14u/include/board/debug_ll.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2015 Antony Pavlov <antonynpavlov@gmail.com>
+ *
+ * This file is part of barebox.
+ * See file CREDITS for list of people who contributed to this project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __INCLUDE_BOARD_DEBUG_LL_H__
+#define __INCLUDE_BOARD_DEBUG_LL_H__
+
+#define DEBUG_LL_UART_ADDR	0xb0000c00
+#define DEBUG_LL_UART_SHIFT	2
+
+/* FIXME: CLK/BPS/DIVISOR are IGNORED */
+#define DEBUG_LL_UART_CLK       1843200
+#define DEBUG_LL_UART_BPS       CONFIG_BAUDRATE
+#define DEBUG_LL_UART_DIVISOR   (DEBUG_LL_UART_CLK / DEBUG_LL_UART_BPS)
+
+#endif /* __INCLUDE_BOARD_DEBUG_LL_H__ */
