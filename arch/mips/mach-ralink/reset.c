dcd57e50c48fad0d9cd6a03a0cc6995c0670b05a
Antony Pavlov <antonynpavlov@gmail.com>
2015-11-21 01:46:17 +0300

    WIP: MIPS: initial Ralink/MediaTek machines support
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

---
+++ arch/mips/mach-ralink/reset.c
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2015 Antony Pavlov <antonynpavlov@gmail.com>
+ *
+ * This file is part of barebox.
+ * See file CREDITS for list of people who contributed to this project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+/**
+ * @file
+ * @brief Resetting an malta board
+ */
+
+#include <common.h>
+#include <io.h>
+#include <init.h>
+#include <restart.h>
+
+/* Reset Control */
+#define SYSC_REG_RESET_CTRL	0x034
+
+#define RSTCTL_RESET_SYSTEM	BIT(0)
+
+static void __noreturn ralink_restart_soc(struct restart_handler *rst)
+{
+	/* rt_sysc_membase = plat_of_remap_node("ralink,mt7620a-sysc"); */
+	void __iomem *rt_sysc_membase = (void *)0xb0000000;
+
+	__raw_writel(RSTCTL_RESET_SYSTEM, rt_sysc_membase + SYSC_REG_RESET_CTRL);
+
+	hang();
+	/* NOTREACHED */
+}
+
+static int restart_register_feature(void)
+{
+	restart_handler_register_fn(ralink_restart_soc);
+
+	return 0;
+}
+coredevice_initcall(restart_register_feature);
