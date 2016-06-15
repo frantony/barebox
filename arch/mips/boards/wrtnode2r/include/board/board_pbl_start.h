a9de7d14c2e08880b8a72710b9261dda700d1338
Antony Pavlov <antonynpavlov@gmail.com>
2016-06-15 16:00:19 +0300

    MIPS: add WRTnode2R board support
    See http://wiki.wrtnode.cc/index.php?title=WRTnode2R/en for details
    
    Build HOWTO
    ===========
    
    ````
    $ git clone -b 20160616.mediatek https://github.com/frantony/barebox
    $ cd barebox
    $ export ARCH=mips
    $ export CROSS_COMPILE=<path to your compiler>/mips-linux-gnu-
    $ make wrtnode2r_defconfig
    $ make -j3
    ````
    
    barebox loadable binary image file is arch/mips/pbl/zbarebox.bin
    
    Starting barebox on WRTnode board
    =================================
    
    1. connect UART and turn board on
    ---------------------------------
    
    ````
    U-Boot 1.1.3 (Jun 25 2015 - 22:31:21)
    
    Board: Ralink APSoC DRAM:  256 MB
    relocate_code Pointer at: 8ffb8000
    flash manufacture id: ef, device id 40 19
    find flash: W25Q256FV
    *** Warning - bad CRC, using default environment
    
    ============================================
    Ralink UBoot Version: 4.3.0.0
    --------------------------------------------
    ASIC 7628_MP (Port5<->None)
    DRAM component: 2048 Mbits DDR, width 16
    DRAM bus: 16 bit
    Total memory: 256 MBytes
    Flash component: SPI Flash
    Date:Jun 25 2015  Time:22:31:21
    ============================================
    icache: sets:512, ways:4, linesz:32 ,total:65536
    dcache: sets:256, ways:4, linesz:32 ,total:32768
    
     ##### The CPU freq = 575 MHZ ####
     estimate memory size =256 Mbytes
    RESET MT7628 PHY!!!!!!
    Please choose the operation:
       1: Load system code to SDRAM via TFTP.
       2: Load system code then write to Flash via TFTP.
       3: Boot system code via Flash (default).
       4: Entr boot command line interface.
       7: Load Boot Loader code then write to Flash via Serial.
       9: Load Boot Loader code then write to Flash via TFTP.
    
    You choosed 4
    
     0
    
    4: System Enter Boot Command Line Interface
    
    U-Boot 1.1.3 (Jun 25 2015 - 22:31:21)
    MT7628 #
    ````
    
    2. load zbarebox.bin via kermit and start it
    --------------------------------------------
    
    On debian install kermit:
    
    ````
    $ sudo apt-get install ckermit
    ````
    
    Add kermit config file ~/.kermrc:
    
    ````
    set carrier-watch off
    set handshake none
    set flow-control none
    robust
    set file type bin
    set rec pack 1000
    set send pack 1000
    set window 5
    ````
    
    Load zbarebox.bin binary image via kermit
    ````
    MT7628 # loadb
    ````
    
    Run barebox:
    
    ````
    MT7628 # go 0x80100000
    pbl_main_entry()
    Switch to console [cs0]
    
    barebox 2016.06.0-00167-g6341acc #23 Thu Jun 16 15:17:57 MSK 2016
    
    Board: WRTnode2R
    Warning: Using dummy clocksource
    malloc space: 0x82000000 -> 0x82ffffff (size 16 MiB)
    environment load /dev/env0: No such file or directory
    Maybe you have to create the partition.
    running /env/bin/init...
    /env/bin/init not found
    barebox:/
    ````
    
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

---
+++ arch/mips/boards/wrtnode2r/include/board/board_pbl_start.h
@@ -0,0 +1,39 @@
+/*
+ * Startup Code for MIPS CPU
+ *
+ * Copyright (C) 2016 Antony Pavlov <antonynpavlov@gmail.com>
+ *
+ * This file is part of barebox.
+ * See file CREDITS for list of people who contributed to this project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#include <asm/pbl_macros.h>
+#include <asm/pbl_nmon.h>
+
+	.macro	board_pbl_start
+	.set	push
+	.set	noreorder
+
+	mips_barebox_10h
+
+	mips_disable_interrupts
+
+	/* cpu specific setup ... */
+	/* ... absent */
+
+	mips_nmon
+
+	copy_to_link_location	pbl_start
+
+	.set	pop
+	.endm
