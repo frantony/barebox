19ec75fd2a24941eb52ae775fbf29dbe971e3218
Antony Pavlov <antonynpavlov@gmail.com>
2017-07-19 14:47:46 +0300

    arch/riscv/lib/barebox.lds.S: adapt for latest toolchain
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- arch/riscv/include/asm/byteorder.h
+++ arch/riscv/include/asm/byteorder.h
@@ -1,12 +1,10 @@
 #ifndef _ASM_RISCV_BYTEORDER_H
 #define _ASM_RISCV_BYTEORDER_H
 
-#if defined(__RISCVEL__)
-#include <linux/byteorder/little_endian.h>
-#elif defined(__RISCVEB__)
+#if defined(__RISCVEB__)
 #include <linux/byteorder/big_endian.h>
 #else
-#error "Unknown endianness"
+#include <linux/byteorder/little_endian.h>
 #endif
 
 #endif /* _ASM_RISCV_BYTEORDER_H */
