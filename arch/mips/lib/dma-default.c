2a273a44cee74a2aa4d459eb835866b696502765
Antony Pavlov <antonynpavlov@gmail.com>
2020-05-05 10:48:19 +0300

    MIPS: add map/unmap_single
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- arch/mips/lib/dma-default.c
+++ arch/mips/lib/dma-default.c
@@ -46,3 +46,19 @@ void dma_sync_single_for_device(dma_addr_t address, size_t size,
 {
 	__dma_sync_mips(address, size, dir);
 }
+
+dma_addr_t dma_map_single(struct device_d *dev, void *ptr, size_t size,
+			  enum dma_data_direction dir)
+{
+	unsigned long addr = virt_to_phys(ptr);
+
+	dma_sync_single_for_device(addr, size, dir);
+
+	return addr;
+}
+
+void dma_unmap_single(struct device_d *dev, dma_addr_t addr, size_t size,
+		      enum dma_data_direction dir)
+{
+	dma_sync_single_for_cpu(addr, size, dir);
+}
